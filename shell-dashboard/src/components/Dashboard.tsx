872e120a00bb57a8312d2655ebe7953d2bd683bb
Barbara Brzic <barbara.brzic97@gmail.com>
2026-03-03 18:28:16 +0100

    add fake authentication in dashboard app

---
+++ shell-dashboard/src/components/Dashboard.tsx
@@ -0,0 +1,19 @@
+import React, {Suspense} from "react";
+import {useOrders} from "../hooks/useOrders.ts";
+
+const ProductsApp = React.lazy(() => import('products/ProductsApp'))
+
+export const Dashboard = ({user, logout}: {user: {name: string}, logout: () => void}) => {
+    const ordersRef = useOrders();
+
+    return (
+        <div>
+            <p>Welcome, {user.name}!</p>
+            <button onClick={() => logout()}>Logout</button>
+            <Suspense fallback={<div>Loading products...</div>}>
+                <ProductsApp/>
+            </Suspense>
+            <div ref={ordersRef}></div>
+        </div>
+    )
+}
