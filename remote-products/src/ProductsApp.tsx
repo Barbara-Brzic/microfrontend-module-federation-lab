bfbb3445e11ae664ab05556c3c0fe07d87e58617
Barbara Brzic <barbara.brzic97@gmail.com>
2026-03-04 11:15:41 +0100

    display products inside virtualized list with loading spinner

--- remote-products/src/ProductsApp.tsx
+++ remote-products/src/ProductsApp.tsx
@@ -1,9 +1,30 @@
+import {useEffect, useState} from "react";
+import {getProducts, type Product} from "@/api.ts";
+import {Spinner} from "@/components/ui/spinner.tsx";
+import {ProductList} from "@/components/ProductList.tsx";
+
 function ProductsApp() {
-  return (
-    <div>
-      Products
-    </div>
-  )
+    const [products, setProducts] = useState<Product[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const response = await getProducts();
+            if (response) {
+                setProducts(response);
+            }
+        }
+
+        fetchProducts().finally(() => setLoading(false));
+    }, [])
+
+    if (loading) {
+        return <Spinner />
+    }
+
+    return (
+        <ProductList products={products} />
+    )
 }
 
 export default ProductsApp
