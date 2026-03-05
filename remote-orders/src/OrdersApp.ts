256f47db1c7ef1f7e49a47675343fdf2b704405c
Barbara Brzic <barbara.brzic97@gmail.com>
2026-03-05 12:09:58 +0100

    install primevue, display order status

--- remote-orders/src/OrdersApp.ts
+++ remote-orders/src/OrdersApp.ts
@@ -1,8 +1,17 @@
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+import 'primeicons/primeicons.css'
 import Orders from './Orders.vue'
 
 function mount(el: string | Element) {
-  createApp(Orders).mount(el)
+  const app = createApp(Orders)
+  app.use(PrimeVue, {
+    theme: {
+      preset: Aura,
+    },
+  })
+  app.mount(el)
 }
 
 export default { mount }
